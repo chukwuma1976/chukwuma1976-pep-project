a9174b762e0324cf33aafafa5d2cece54fdc1e83
Chukwuma Anyadike <chukwuma.anyadike@gmail.com>
2023-09-09 02:40:26 +0000

    created controller routes in controller class

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -14,20 +14,67 @@ public class SocialMediaController {
      * suite must receive a Javalin object from this method.
      * @return a Javalin app object which defines the behavior of the Javalin controller.
      */
+
+    // future service objects
+    // AccountService accountService = new AccountService();
+    // MessageService messageService = new MessageService();
+
     public Javalin startAPI() {
         Javalin app = Javalin.create();
-        app.get("example-endpoint", this::exampleHandler);
+        // app.get("example-endpoint", this::exampleHandler);
 
+        //user registration controller route
+        app.post("/register", this::registerHandler);
+
+        //login controller route
+        app.post("/login", this::loginHandler);
+
+        //message controllers routes
+        app.get("/messages", this::getAllMessagesHandler);
+        app.post("/messages", this::addMessageHandler);
+        app.get("/messages{message_id}", this::getMessageByIdHandler);
+        app.delete("/messages{message_id}", this::deleteMessageByIdHandler);
+        app.patch("/messages{message_id}", this::updateMessageByIdHandler);
+        app.get("/accounts/{account_id}/messages", this::getMessagesByAccountIDHandler);
+
+        // app.start(8080);
         return app;
     }
 
     /**
-     * This is an example handler for an example endpoint.
+     * These are my handlers, the names speak for themselves
      * @param context The Javalin Context object manages information about both the HTTP request and response.
      */
-    private void exampleHandler(Context context) {
+    private void registerHandler(Context context) {
         context.json("sample text");
     }
 
+    private void loginHandler(Context context) {
+        context.json("sample text");
+    }
+
+    private void getAllMessagesHandler(Context context) {
+        context.json("sample text");
+    }
+
+    private void addMessageHandler(Context context) {
+        context.json("sample text");
+    }
+
+    private void getMessageByIdHandler(Context context) {
+        context.json("sample text");
+    }
+
+    private void deleteMessageByIdHandler(Context context) {
+        context.json("sample text");
+    }    
+
+    private void updateMessageByIdHandler(Context context) {
+        context.json("sample text");
+    }  
+
+    private void getMessagesByAccountIDHandler(Context context) {
+        context.json("sample text");
+    }  
 
 }
